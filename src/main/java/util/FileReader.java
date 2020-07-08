3eec8339c8cb7f2bab9673f876a24a633c3d9f6f
Rostyslav Ludchenko <skillet367@icloud.com>
2020-07-08 17:16:56 +0300

    added test to check if file exists

--- src/main/java/util/FileReader.java
+++ src/main/java/util/FileReader.java
@@ -8,11 +8,11 @@ import java.nio.file.Paths;
 public class FileReader {
     private static final String PATH = "D:\\reviews\\Reviews.csv";
 
-    public static void main(String[] args) throws IOException {
+    public void main(String[] args) throws IOException {
         readFileByString(PATH);
     }
 
-    public static void readFileByString(String path) throws IOException {
+    public void readFileByString(String path) throws IOException {
         Files.lines(Paths.get(path)).forEach(System.out::println);
     }
 }
